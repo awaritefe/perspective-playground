1391961125fb57b73c4cdd84ea80b5103ee0ba3b
Daniel <daniel.awaritefe@fatbeehive.com>
2023-04-04 12:21:46 +0100

    hooked up asyc button to copy the css to clipboard

--- app.js
+++ app.js
@@ -6,5 +6,32 @@ const vm = Vue.createApp({
 			rotateY: 0,
 			rotateZ: 0
 		}
+	},
+	computed: {
+		box() {
+			return {
+				transform: `
+					perspective(${this.perspective}px)
+					rotateX(${this.rotateX}deg)
+					rotateY(${this.rotateY}deg)
+					rotateZ(${this.rotateZ}deg)
+				`
+			}
+		}
+	},
+	methods: {
+		reset() {
+			this.perspective = 100
+			this.rotateX = 0
+			this.rotateY = 0
+			this.rotateZ = 0
+		},
+		async copy() {
+			const text = `transform:${this.box.transform};`
+			await navigator.clipboard.writeText(text)
+			
+			alert('CSS copied to clipboard!')
+		}
 	}
+	
 }).mount('#app');
